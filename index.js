d30e49c57eb4b9a8f0969390cda874bc33b1c8f1
Bhaskar KC <kcbhaskar1328@gmail.com>
2025-04-09 23:26:17 +0545

    Fix: switch to bcryptjs and add SSL configuration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
@@ -32,6 +32,7 @@ const db = new pg.Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 db.connect();
 
